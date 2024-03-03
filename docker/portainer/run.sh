5cd12fc396b4b27089e34f9bb879bd2b0f110186
Kiril Tonev <tonev@fastmail.com>
2024-03-03 12:32:36 +0100

    Fix portainer command.

--- docker/portainer/run.sh
+++ docker/portainer/run.sh
@@ -1,1 +1,8 @@
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/zfs/appdata/portainer:/data portainer/portainer-ce:latest
+docker run -d \
+  --name portainer \
+  --restart=unless-stopped \
+  -p 9000:9000 \
+  -p 9443:9443 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /mnt/zfs/appdata/portainer:/data \
+  portainer/portainer-ce:latest
