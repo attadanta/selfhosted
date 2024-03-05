38d1e991cab7bc04c927437bfbe713e47eb0db0d
Kiril Tonev <tonev@fastmail.com>
2024-03-05 07:36:43 +0100

    Improve file names.

--- ansible/build.sh
+++ ansible/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t ansibletest .
-docker run -it --rm ansibletest /bin/bash -c "ansible-playbook -i hosts --limit localhost run.yml"
+docker run -it --rm ansibletest /bin/bash -c "ansible-playbook -i hosts --limit localhost site.yml --skip-tags 'personal'"
