14d44577637e41c3b18b41812c0726293b085857
Kiril Tonev <tonev@fastmail.com>
2024-03-08 20:04:53 +0100

    Try to figure out how to source bash scripts.

--- ansible/build.sh
+++ ansible/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t ansibletest .
-docker run --rm ansibletest /bin/bash -c "ansible-playbook -i hosts --limit localhost site.yml --skip-tags 'personal'"
+docker run --rm ansibletest /bin/bash -c "ansible-playbook -vvv -i hosts --limit localhost site.yml"
