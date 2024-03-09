dff8bc512461328d3bbd57b746058438b40772e9
Kiril Tonev <tonev@fastmail.com>
2024-03-09 13:56:22 +0100

    Change shell to zsh, install ohmyzsh and stow dotfiles.

--- ansible/build.sh
+++ ansible/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-docker build -t ansibletest .
-docker run --rm ansibletest /bin/bash -c "ansible-playbook -vvv -i hosts --limit localhost site.yml"
+docker build --target build -t ansibleinit .
+docker run --rm ansibleinit /bin/bash -c "ansible-playbook -vvv -i hosts --limit localhost site.yml"
+
+docker build --target run -t ansibleready .
